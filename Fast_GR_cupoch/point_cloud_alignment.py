fa3791ae4a7ec6ffbac6be4fcdb1cfd3e0127222
Taru Rustagi <taru0904@umd.edu>
2021-07-13 01:00:38 -0400

    o3d point cloud alignment test:

--- Fast_GR_cupoch/point_cloud_alignment.py
+++ Fast_GR_cupoch/point_cloud_alignment.py
@@ -1,20 +1,7 @@
-import cupoch as cph
 import numpy as np
 import copy
 import time
 import open3d as o3d
-
-
-
-
-def draw_registration_result(source, target, transformation):
-    source_temp = copy.deepcopy(source)
-    target_temp = copy.deepcopy(target)
-    source_temp.paint_uniform_color([1, 0.706, 0])
-    target_temp.paint_uniform_color([0, 0.651, 0.929])
-    source_temp.transform(transformation)
-    cph.visualization.draw_geometries([source_temp, target_temp])
-
 
 def preprocess_point_cloud(pcd, voxel_size):
     print(":: Downsample with a voxel size %.3f." % voxel_size)
@@ -47,31 +34,39 @@ def prepare_dataset(voxel_size):
     return source, target, source_down, target_down, source_fpfh, target_fpfh
 
 
+voxel_size = 0.5  # means 5cm for this dataset
+source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(
+    voxel_size)
+
 def execute_fast_global_registration(source_down, target_down, source_fpfh,
                                      target_fpfh, voxel_size):
     distance_threshold = voxel_size * 0.5
     print(":: Apply fast global registration with distance threshold %.3f" \
             % distance_threshold)
-    result = cph.registration.registration_fast_based_on_feature_matching(
+    result = o3d.pipelines.registration.registration_fast_based_on_feature_matching(
         source_down, target_down, source_fpfh, target_fpfh,
-        cph.registration.FastGlobalRegistrationOption(
+        o3d.pipelines.registration.FastGlobalRegistrationOption(
             maximum_correspondence_distance=distance_threshold))
     return result
 
 
-if __name__ == "__main__":
-    cph.utility.set_verbosity_level(cph.utility.Debug)
+def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.4
+    print(":: Point-to-plane ICP registration is applied on original point")
+    print("   clouds to refine the alignment. This time we use a strict")
+    print("   distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_icp(
+        source, target, distance_threshold, result_ransac.transformation,
+        o3d.pipelines.registration.TransformationEstimationPointToPlane())
+    return result
 
-    voxel_size = 0.5  # means 5cm for the dataset
-    source, target, source_down, target_down, source_fpfh, target_fpfh = \
-            prepare_dataset(voxel_size)
 
-    start = time.time()
-    result_fast = execute_fast_global_registration(source_down, target_down,
-                                                   source_fpfh, target_fpfh,
-                                                   voxel_size)
-    print("Fast global registration took %.3f sec.\n" % (time.time() - start))
-    print(result_fast)
-    print(result_fast.transformation)
-    #draw_registration_result(source_down, target_down,
-    #                         result_fast.transformation)
+start = time.time()
+result_fast = execute_fast_global_registration(source_down, target_down,
+                                               source_fpfh, target_fpfh,
+                                               voxel_size)
+print("Fast global registration took %.3f sec.\n" % (time.time() - start))
+print(result_fast)
+result_icp = refine_registration(source, target, source_fpfh, target_fpfh,
+                                 voxel_size)
+print(result_icp)
